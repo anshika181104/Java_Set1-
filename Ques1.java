1ca5d3dfddfb95b69643caced69d274beb3137cb
anshika181104 <anuverma181104@gmail.com>
2024-08-07 22:42:52 +0530

    code for sum of even numbers

--- Ques1.java
+++ Ques1.java
@@ -1,5 +1,17 @@
-package Java_Set1-;
-
-public class Ques1 {
-    
+/* Given a number N, print sum of all
+     even numbers from 1 to N. */
+     
+      public class Ques1{
+public static void main(String[] args) {
+    int N = 20;
+    int sum = 0;
+    int i;
+    for(i = 1; i <= N; i++){
+        if( i % 2 == 0){
+            sum = sum + i;
+        }
+    }
+    System.out.println("sum of even numbers are " + sum);
 }
+     }
+    
