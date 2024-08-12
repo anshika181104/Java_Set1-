2cea84f3aa0f00f3a34d7df564ed3a152b3e0ee1
anshika181104 <anuverma181104@gmail.com>
2024-08-12 20:44:08 +0530

    added

--- Ques2.java
+++ Ques2.java
@@ -1,3 +1,16 @@
-public class Ques2 {
-    
-}
+/* Given three values - Start Fahrenheit Value (S), End Fahrenheit value (E)
+ and Step Size (W), you need to convert all Fahrenheit values
+  from Start to End at the gap of W, into their corresponding Celsius
+   values and print the table.  (c=(5*(S-32))/9;)  */
+
+   public class Ques2{
+    public static void main(String[] args) {
+        double E = 85.5;
+        double W = 2.0;
+        for (double S = 78.8; S <= E; S = S+W){
+            System.out.println(S);
+            double celsiusvalue = ((5*(S-32)/9));
+            System.out.println("celsius value: " + celsiusvalue);
+        }
+    }
+   }
