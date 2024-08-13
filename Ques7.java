b5f7445d465d1d79c3591f147a3e8a366854ef8c
anshika181104 <anuverma181104@gmail.com>
2024-08-13 21:55:07 +0530

    added ques7 code

--- Ques7.java
+++ Ques7.java
@@ -1,3 +1,19 @@
-public class Ques7 {
-    
-}
+/* Write a program to find x to the power n (i.e. x^n). 
+Take x and n from the user. You need to print the answer.
+ */
+import java.util.Scanner;
+ public class Ques7{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the base (x): ");
+        double x = scanner.nextDouble();
+
+        System.out.println("Enter the exponent (n): ");
+        double n = scanner.nextDouble();
+
+        double result = Math.pow(x, n);
+
+        System.out.println("The result of " + x + " to the power of " + n + " is: " + result);
+    }
+    }
+ 
